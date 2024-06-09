b54365b3d5875808a9f98b24c2772c336d822132
Surya Vasudev <suryavasudev05@gmail.com>
2024-06-09 17:48:45 -0400

    enhanced filepicker functionality and made the UX a bit better

--- main.py
+++ main.py
@@ -7,36 +7,56 @@ import pikepdf
 
 
 filepath = ""
-savepath = pathlib.Path.home()
+savepath = str(pathlib.Path.home()) + "/Desktop/"
+
 def getfile():
     global filepath
     filepath = filedialog.askopenfilename()
+    filepath_label.set(filepath)
 
 def getdir():
     global savepath
     savepath = filedialog.askdirectory()
     savepath += "/"
+    save_path_label.set(savepath)
+    
+def retrieve_input():
+    global passd
+    passd = textbox.get("1.0","end-1c").strip().split(",")
+    root.destroy()
 
 root = Tk()
-root.geometry('300x300')
+root.geometry('500x300')
 root.eval('tk::PlaceWindow . center')
 
+save_path_label = StringVar()
+save_path_label.set(savepath)
+
+filepath_label = StringVar()
+filepath_label.set(filepath)
+
 btn = Button(root, text = 'Done', 
-                command = root.destroy)
+                command = lambda: retrieve_input())
 
 btn.pack(side = 'bottom')     
 
-label = Label(root,text="Enter Password below:")
+label = Label(root,text="Enter Password/s below: (Separate passwords by ',' if multiple)")
 label.pack()
-textbox = Entry(root)
-passd = textbox.get().strip().split(",")
+
+textbox = Text(root, height=2, width=30)
 textbox.pack()
 
 open_button = Button(root, text="Open File/Folder to decrypt", command=getfile)
 open_button.pack()
 
+current_chosen_file_or_folder_label = Label(root, textvariable= filepath_label)
+current_chosen_file_or_folder_label.pack()
+
 choose_button = Button(root, text="Choose the location to save the file", command=getdir)
 choose_button.pack()
+
+current_save_location_label = Label(root, textvariable= save_path_label)
+current_save_location_label.pack()
 
 root.mainloop()
 
@@ -44,6 +64,8 @@ number_of_passwords = len(passd)
 
 isDir = os.path.isdir(filepath)
 
-passd = "144011999"
-with pikepdf.open('input.pdf',password=passd) as pdf:
-    pdf.save(savepath + 'output.pdf')
+if passd[0] != '':
+
+    if not isDir:
+        with pikepdf.open(filepath,password=passd[0]) as pdf:
+            pdf.save(savepath + 'output.pdf')
